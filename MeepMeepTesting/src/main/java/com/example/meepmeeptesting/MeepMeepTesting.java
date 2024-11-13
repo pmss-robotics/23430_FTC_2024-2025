37391238bce9868ccd6f4fca034555ad70c54392
Johnny <johnnyhanj@gmail.com>
2024-11-12 22:40:04 -0800

    some auto stuff

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -2,6 +2,7 @@ package com.example.meepmeeptesting;
 
 
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -15,15 +16,19 @@ public class MeepMeepTesting {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 0, 0))
-                .lineToX(30)
-                .turn(Math.toRadians(90))
-                .lineToY(30)
-                .turn(Math.toRadians(90))
-                .lineToX(0)
-                .turn(Math.toRadians(90))
-                .lineToY(0)
-                .turn(Math.toRadians(90))
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-38, -61.5, Math.PI/2))
+                .splineTo(new Vector2d(-54.5, -54.5), 5*Math.PI/4)
+                .waitSeconds(3)
+                .splineToSplineHeading(new Pose2d(-58, -45, Math.PI/2), Math.PI/2)
+                .waitSeconds(2)
+                .splineToLinearHeading(new Pose2d(-54.5, -54.5, 5*Math.PI/4), Math.PI/2)
+                .waitSeconds(3)
+                .splineToLinearHeading(new Pose2d(-52, -45, Math.toRadians(75)), Math.toRadians(75))
+                .waitSeconds(2)
+                .splineToLinearHeading(new Pose2d(-54.5, -54.5, 5*Math.PI/4), Math.PI/2)
+                .waitSeconds(3)
+                .splineToLinearHeading(new Pose2d(-52, -45, Math.toRadians(75)), Math.toRadians(75))
+                .splineToLinearHeading(new Pose2d(-24, -10, 0), 0)
                 .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
