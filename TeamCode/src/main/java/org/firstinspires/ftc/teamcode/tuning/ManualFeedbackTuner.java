2789e3de5f3d2ce975f9616d06de7693536afc40
Johnny <johnnyhanj@gmail.com>
2024-12-06 14:28:45 -0800

    changed feedback tuner for strafing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/ManualFeedbackTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/ManualFeedbackTuner.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.tuning;
 
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
@@ -19,8 +20,8 @@ public final class ManualFeedbackTuner extends LinearOpMode {
             while (opModeIsActive()) {
                 Actions.runBlocking(
                         drive.actionBuilder(new Pose2d(0, 0, 0))
-                                .lineToX(DISTANCE)
-                                .lineToX(0)
+                                .strafeTo( new Vector2d(0, DISTANCE)) //change back to lineToX for forwards and backwards
+                                .strafeTo(new Vector2d(0,0))
                                 .build());
             }
         } else if (TuningOpModes.DRIVE_CLASS.equals(MecanumDrive.class)) {
