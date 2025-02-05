b657ee3475f958e6e290df730fea89aa2a1a6a8b
digcheese <jeffxue269@gmail.com>
2025-02-05 15:10:10 -0800

    added updated vision (still not complete)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/VisionSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/VisionSubsystem.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import static org.firstinspires.ftc.teamcode.vision.AutoAlignProcessor.AnalyzedStone;
 import static java.lang.Thread.sleep;
 
 import android.graphics.Bitmap;
@@ -20,36 +21,41 @@ import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.Exposur
 import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainControl;
 import org.firstinspires.ftc.robotcore.external.stream.CameraStreamSource;
 import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
-import org.firstinspires.ftc.teamcode.vision.SampleVisionProcessor;
+import org.firstinspires.ftc.teamcode.vision.AutoAlignProcessor;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.VisionProcessor;
-import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
-import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 import org.opencv.android.Utils;
 import org.opencv.core.Mat;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
+
+
 
 @Config
 public class VisionSubsystem extends SubsystemBase {
 
     public static int EXPOSURE_MS = 6;
     public static int GAIN = 190;
-    private final AprilTagProcessor aprilTag;
-    private final SampleVisionProcessor sampleVisionProcessor;
+    //private final AprilTagProcessor aprilTag;
+    private final AutoAlignProcessor sampleDetection;
     private final VisionPortal visionPortal;
     private Telemetry telemetry;
 
     /**
      * All vision related logic will be done here including april tags, tensorflow, and opencv
+     *
      * @param hardwareMap
      */
     public VisionSubsystem(HardwareMap hardwareMap, Telemetry telemetry) throws InterruptedException {
         this.telemetry = telemetry;
         // TODO sample code!!!! re-implement in the field.
         final CameraStreamProcessor dashboard = new CameraStreamProcessor();
-        sampleVisionProcessor = new SampleVisionProcessor();
+        sampleDetection = new AutoAlignProcessor();
+        /*
         aprilTag = new AprilTagProcessor.Builder()
                 .setTagLibrary(AprilTagGameDatabase.getCurrentGameTagLibrary())
                 .setDrawTagID(true)
@@ -57,12 +63,16 @@ public class VisionSubsystem extends SubsystemBase {
                 .setDrawAxes(true)
                 .setDrawCubeProjection(true)
                 .build();
+
+         */
         visionPortal = new VisionPortal.Builder()
                 .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
-                .addProcessors(dashboard, aprilTag, sampleVisionProcessor)
+                .addProcessors(dashboard, sampleDetection)
                 .setCameraResolution(new Size(640, 480))
                 .setStreamFormat(VisionPortal.StreamFormat.MJPEG) // worse compression than the default but faster
                 .build();
+
+        visionPortal.setProcessorEnabled(sampleDetection, false);
 
         // some hacky logic to make sure the camera is fully ready before we put in our configs
         // otherwise it'll just crash, which is bad
@@ -85,8 +95,37 @@ public class VisionSubsystem extends SubsystemBase {
             gainControl.setGain(GAIN);
             sleep(20);
         }
-        FtcDashboard.getInstance().startCameraStream(dashboard, 30);
+        FtcDashboard.getInstance().startCameraStream(dashboard, 15);
+        //FIXME: disable during comp visionPortal.stopLiveView();
+
     }
+
+    public void enableDetection(boolean enabled) {
+        visionPortal.setProcessorEnabled(sampleDetection, enabled);
+    }
+
+
+    public double getSampleAngle() {
+        double angle = 0;
+        AnalyzedStone closest = null;
+        List<AnalyzedStone> detections = sampleDetection.getDetectedStones();
+        for ( AnalyzedStone sample : detections) {
+            // find the sample closest to the claw
+            telemetry.addData("Sample: " + detections.indexOf(sample), " "+ sample.color);
+            telemetry.addData("Sample: " + detections.indexOf(sample), " " + sample.angle);
+
+            for (int i = 0; i < sample.tvec.rows(); i++) {
+                for (int j = 0; j < sample.tvec.cols(); j++) {
+                    telemetry.addData("Sample: " + detections.indexOf(sample), " term 1 " + Arrays.toString(sample.tvec.get(i, j)));
+
+                }
+
+            }
+        }
+        if(Objects.isNull(closest)) return 666;
+        return angle;
+    }
+
 
     public static class CameraStreamProcessor implements VisionProcessor, CameraStreamSource {
         private final AtomicReference<Bitmap> lastFrame =
