28ad24cad1c5e602f90fb0f91c05e449ef397006
Johnny <johnnyhanj@gmail.com>
2025-02-10 13:18:09 -0800

    drive speed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/MecanumDrive.java
@@ -73,9 +73,9 @@ public class MecanumDrive {
         public double kA = 0.031;
 
         // path profile parameters (in inches)
-        public double maxWheelVel = 60;
-        public double minProfileAccel = -60;
-        public double maxProfileAccel = 60;
+        public double maxWheelVel = 70;
+        public double minProfileAccel = -70;
+        public double maxProfileAccel = 70;
 
         // turn profile parameters (in radians)
         public double maxAngVel = Math.PI; // shared with path
