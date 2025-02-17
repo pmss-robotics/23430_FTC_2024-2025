dc69a39a57e5fefa80e59075a79063ed9e7f6126
Johnny <johnnyhanj@gmail.com>
2025-02-17 10:18:21 -0800

    sweeper set position for auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/IntakeSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/IntakeSubsystem.java
@@ -144,6 +144,19 @@ public class IntakeSubsystem extends SubsystemBase {
         }
     }
 
+    public void putSweeperDown (boolean down) {
+        sweeperDown = down;
+        if (down) {
+            sweep.setPosition(scale(sDown));
+        } else {
+            sweep.setPosition(scale(sHome));
+        }
+    }
+
+    public void setSweeper (int position) {
+        sweep.setPosition(scale(position));
+    }
+
     public void openIntakeClaw() {
         claw.setPosition(scale(cOpen));
         intakeOpen = true;
